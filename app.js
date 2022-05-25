fb340ed59bf05ebb452e6ea86802843b5589c0b6
BriWidowski <33921803+briwidowski@users.noreply.github.com>
2022-05-25 18:18:40 -0500

    Add new input items into javascript

--- app.js
+++ app.js
@@ -1,39 +1,79 @@
+
+const input = document.querySelector('.add');
+const tasksList = document.querySelector('.tasksList');
+const actionBtns = document.getElementsByClassName('actionButton');
+const actionBtnsArray = Array.prototype.slice.call(actionBtns);
+
+let newTask = "";
+let editBtn = "";
+
 window.addEventListener('load', () => {
-    const input = document.querySelector('.add');
-    const tasksList = document.querySelector('.tasksList');
-    
-    input.addEventListener('keypress', function (e) {
-        if (e.key === 'Enter') {
-            e.preventDefault();
-            const task = input.value;
-
-            // Create a new li element for input
-            const newTask = document.createElement('li');
-            newTask.innerHTML = task;
-
-            // Create a div for the edit and delete buttons
-            const actionBtns = document.createElement('div');
-            actionBtns.classList.add('action-buttons');
-
-
-            const edit = document.createElement('button');
-            edit.classList.add('edit');
-            edit.innerHTML = "Edit";
-
-            const deleteBtn = document.createElement('button');
-            deleteBtn.classList.add('delete');
-            deleteBtn.innerHTML = "Delete";
-
-
-            actionBtns.appendChild(edit);
-            actionBtns.appendChild(deleteBtn);
-
-            newTask.appendChild(actionBtns);
-            tasksList.appendChild(newTask);
-
-            input.value = "";
-
-        }
-    })
+    input.focus();
+    console.log(actionBtnsArray);
 })
+    
+input.addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        e.preventDefault();
+        const task = input.value;
 
+        if (task == "") {
+            alert("Type something to add to the list!");
+            return;
+        }
+        // Create a new li element for input
+        newTask = document.createElement('li');
+        newTask.classList.add('text-content');
+        newTask.innerHTML = task;
+
+        // Create a div for the edit and delete buttons
+        const btnsDiv = document.createElement('div');
+        btnsDiv.classList.add('action-buttons');
+
+
+        editBtn = document.createElement('button');
+        editBtn.classList.add("edit","actionButton");
+        editBtn.innerHTML = "Edit";
+
+        const deleteBtn = document.createElement('button');
+        deleteBtn.classList.add("delete", "actionButton");
+        deleteBtn.innerHTML = "Delete";
+
+
+        btnsDiv.appendChild(editBtn);
+        actionBtnsArray.push(editBtn);
+        btnsDiv.appendChild(deleteBtn);
+        actionBtnsArray.push(deleteBtn);
+
+        newTask.appendChild(btnsDiv);
+        tasksList.appendChild(newTask);
+
+        input.value = "";
+        console.log(actionBtnsArray);
+    }
+})
+ 
+
+// actionBtnsArray.forEach(function (btn) {
+//     btn.addEventListener('click', function(e){
+//         const styles = e.currentTarget.classList;
+//         if (styles.contains('edit')) {
+//             console.log("Yup");
+//         } 
+//         else if (styles.contains('delete')) {
+//             console.log("nope");
+//         }
+//     });
+// })
+
+for (var i = 0; i < actionBtnsArray.length; i++) {
+    actionBtnsArray[i].addEventListener('click', function() {
+        if (actionBtnsArray[i].classList.contains("edit")) {
+            console.log("Yup");
+        }
+        else if (actionBtnsArray[i].classList.contains("delete")) {
+            console.log("Nope");
+        }
+    });
+   
+  }
