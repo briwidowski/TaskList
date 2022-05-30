e053853ee20dfe1de0748ca5261d7c380432cc6d
BriWidowski <33921803+briwidowski@users.noreply.github.com>
2022-05-30 02:41:34 -0500

    Clean up and rewrite Javascript code

--- app.js
+++ app.js
@@ -1,85 +1,79 @@
 
+// Selectors
 const input = document.querySelector('.add');
 const tasksList = document.querySelector('.tasksList');
-const actionBtns = document.getElementsByClassName('actionButton');
-const actionBtnsArray = Array.prototype.slice.call(actionBtns);
+let task = "";
+let target = "";
+let listItemParent = "";
 
-let newTask = "";
-let editBtn = "";
 
-window.addEventListener('load', () => {
-    input.focus();
-    console.log(actionBtnsArray);
-})
-    
-input.addEventListener('keypress', function (e) {
+
+// Functions and Event Listeners
+input.addEventListener('keypress', function(e) {
     if (e.key === 'Enter') {
-        e.preventDefault();
-        const task = input.value;
-
-        if (task == "") {
+        if (input.value != "") {
+            e.preventDefault();
+            task = input.value;
+            addTask();
+        } else {
             alert("Type something to add to the list!");
             return;
         }
-        // Create a new li element for input
-        newTask = document.createElement('li');
-        newTask.classList.add('text-content');
-        newTask.innerHTML = task;
-
-        // Create a div for the edit and delete buttons
-        const btnsDiv = document.createElement('div');
-        btnsDiv.classList.add('action-buttons');
+    } 
+});
 
 
-        editBtn = document.createElement('button');
-        editBtn.classList.add("edit","actionButton");
-        editBtn.innerHTML = "Edit";
+function addTask() {
+    // Create a new li element for input
+    const newTask = document.createElement('li');
+    newTask.classList.add('text-content');
+    newTask.innerHTML = task;
 
-        const deleteBtn = document.createElement('button');
-        deleteBtn.classList.add("delete", "actionButton");
-        deleteBtn.innerHTML = "Delete";
+    // Create a div for the edit and delete buttons
+    const btnsDiv = document.createElement('div');
+    btnsDiv.classList.add('action-buttons');
 
 
-        btnsDiv.appendChild(editBtn);
-        actionBtnsArray.push(editBtn);
-        btnsDiv.appendChild(deleteBtn);
-        actionBtnsArray.push(deleteBtn);
+    const editBtn = document.createElement('button');
+    editBtn.classList.add("edit","actionButton");
+    editBtn.innerHTML = "Edit";
 
-        newTask.appendChild(btnsDiv);
-        tasksList.appendChild(newTask);
+    const deleteBtn = document.createElement('button');
+    deleteBtn.classList.add("delete", "actionButton");
+    deleteBtn.innerHTML = "Delete";
 
-        input.value = "";
-        return newTask;
-        console.log(actionBtnsArray);
 
+    btnsDiv.appendChild(editBtn);
+    //actionBtnsArray.push(editBtn);
+    btnsDiv.appendChild(deleteBtn);
+    //actionBtnsArray.push(deleteBtn);
+
+    newTask.appendChild(btnsDiv);
+    tasksList.appendChild(newTask);
+
+    input.value = "";
+
+    editBtn.addEventListener('click', editTask);
+    deleteBtn.addEventListener('click', deleteTask);
+}
+
+function editTask(e) {
+    target = e.target;
+    if (target.innerHTML.toLowerCase() == "edit") {
+        listItemParent = target.parentElement.parentElement;
+        listItemParent.removeAttribute("readonly");
+        listItemParent.focus();
+        target.innerHTML = "Save";
+        console.log("Yup");
+    } else {
+        target.innerHTML = "Edit";
     }
-})
- 
 
-actionBtnsArray.forEach(function (btn) {
-    btn.addEventListener('click', function(e){
-        const target = e.target;
-        const listItemParent = target.parentElement.parentElement;
-        const styles = e.currentTarget.classList;
-        if (styles.contains('edit')) {
-            if (target.innerHTML.toLowerCase() == "edit") {
-                listItemParent.removeAttribute("readonly");
-                listItemParent.focus();
-                let item = listItemParent.innerHTML;
-                let itemInput = document.createElement('input');
-                itemInput.type = 'text';
-                itemInput.value = item;
-                itemInput.classList.add("edit","actionButton");
-                listItemParent.remove();
-                target.innerHTML = "Save";
-                console.log("Yup");
-            } else {
-                target.innerHTML = "Edit";
-            };
-        } 
-        else if (styles.contains('delete')) {
-            listItemParent.remove();
-            console.log("nope");
-        }
-    });
-})
+}
+
+function deleteTask(e) {
+    target = e.target;
+    listItemParent = target.parentElement.parentElement;
+    listItemParent.remove();
+    console.log("nope");
+}
